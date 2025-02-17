3590ae07d009faba11fc70d249123aae23b33796
Yannick Bahé <yannick.bahe@nottingham.ac.uk>
2025-02-17 12:38:29 +0000

    Added nightingale.py

---
+++ nightingale.py
@@ -0,0 +1,37 @@
+"""Main script of NIGHTINGALE.
+
+Started 17 Feb 2025.
+"""
+
+import numpy as np
+import tools
+import timestamp as ts
+import params
+
+
+def main():
+	"""Main function of Nightingale, processes one snapshot.
+
+	Parses the parameter file and command-line options, reads data,
+	processes individual subhaloes, gathers results, derives output,
+	writes output.
+	"""
+
+    # Parse command-line options:
+    args = params.parse_arguments()
+
+    # Parse input parameter file:
+    par = params.parse_parameter_file(args)
+
+    # Check whether any parameters in the param file should be overriden by
+    # command-line arguments
+    params.override_parameters(par, args)
+        
+    # Set derived parameters and perform consistency checks:
+    params.setup(par)
+
+    
+
+if __name__ == "__main__":
+	main()
+	print("Done!")
