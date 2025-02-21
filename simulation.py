0419e73f52b3a7272a4794cb40e708a3cdbc8f70
Yannick Bahé <yannick.bahe@nottingham.ac.uk>
2025-02-21 14:00:27 +0100

    Added stub of redshift loader to simulation.py

--- simulation.py
+++ simulation.py
@@ -4,6 +4,14 @@ Started 18 Feb 2025.
 """
 
 class Simulation:
-	
+	"""Class to hold basic information about a simulation.
+
+	This is mostly used to attach references to the currently used snapshots.
+	"""
+
 	def __init__(self, par):
 		self.par = par
+
+	def load_redshifts(self, par):
+		"""Load the redshift information for the simulation."""
+		pass
