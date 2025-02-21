501fbe70bb98b253775f622a20c77a44187260f4
Yannick Bahé <yannick.bahe@nottingham.ac.uk>
2025-02-21 14:01:16 +0100

    Updated snapshot particles

--- particles.py
+++ particles.py
@@ -3,6 +3,9 @@
 Started 17 Feb 2025
 """
 
+import numpy as np
+from pdb import set_trace
+
 import unbinding
 
 class ParticlesBase:
@@ -16,10 +19,63 @@ class SnapshotParticles(ParticlesBase):
 		self.snapshot = snapshot
 		self.par = snapshot.par
 
+	def get_property(self, name, indices=None):
+		"""Retrieve a named particle property.
+
+		Parameters
+		----------
+		name : str
+			The name of the attribute holding the property.
+		indices : ndarray(int) or None, optional
+			The indices of particles whose property should be retrieved. If
+			None (default), all particles are loaded.
+
+		Returns
+		-------
+		data : ndarray
+			The requested property for the required particles.
+		"""
+		try:
+			attr = getattr(self, name)
+		except AttributeError:
+			print(f"Desired particle property '{name}' is not loaded!")
+			set_trace()
+
+		if indices is None:
+			return attr
+		else:
+			return attr[indices]
+
 class GalaxyParticles(ParticlesBase):
 
 	def __init__(self, galaxy):
-		pass
+		self.num_part = None
+
+	def check_number_consistency(self, quant):
+		"""Check that the number of elements agrees with internal size."""
+		if self.num_part is not None:
+			if len(quant) != self.num_part:
+				print(f"Requested adding {len(quant)} elements to source, but "
+					  f"there are {self.num_part} particles!")
+				set_trace()
+		else:
+			self.num_part = len(quant)
+
+	def set_r(self, r):
+		self.check_number_consistency(r)
+		self.r = r.astype(np.float64)
+
+	def set_v(self, v):
+		self.check_number_consistency(v)
+		self.v = v.astype(np.float64)
+
+	def set_m(self, m):
+		self.check_number_consistency(m)
+		self.m = m.astype(np.float32)
+
+	def set_u(self, u):
+		self.check_number_consistency(u)
+		self.u = u.astype(np.float32)
 
 	def unbind(self):
 		"""Perform the unbinding procedure on the stored particles."""
