33d24214c30a94ac3871dc7adc4454a0b6ebe949
dc-bahe1@cosma.dur.ac.uk <bahe@strw.leidenuniv.nl>
2025-03-04 16:36:05 +0000

    Changed io.py to ion.py

--- ion.py
+++ ion.py
@@ -3,14 +3,14 @@
 def form_nightingale_property_file(par, isnap):
     """Form the property file name for a given snapshot."""
     catalogue_name = par['Output']['CatalogueName']
-    catalogue_name.replace('XXX', f'{isnap:04d}')
+    catalogue_name = catalogue_name.replace('XXXX', f'{isnap:04d}')
     return par['Output']['Directory'] + catalogue_name
 
 
 def form_nightingale_id_file(par, isnap):
     """Form the subhalo ID file name for a given snapshot."""
     id_name = par['Output']['IDFileName']
-    id_name.replace('XXX', f'{isnap:04d}')
+    id_name = id_name.replace('XXXX', f'{isnap:04d}')
     return par['Output']['Directory'] + id_name
 
 class Output:
