c7f06fa67ad02f7d518075b6e3a2eb2d96218022
dc-bahe1@cosma.dur.ac.uk <bahe@strw.leidenuniv.nl>
2025-03-05 21:14:59 +0000

    Modified Hubble value reporting

--- cosmo.py
+++ cosmo.py
@@ -10,13 +10,10 @@ cosmology = Planck18
 def compute_hubble_z(redshift):
     """Compute the Hubble factor for the specified redshift."""
     hubble_z = cosmology.H(redshift)
-
-    print("Determined H(z) = {:.2f}, epsilon(z) = {:.2f} kpc." 
-      .format(self.hubble_z, self.epsilon*1e3))
-    if not self.hubble_z.unit == 'km / (s Mpc)':
+    if not hubble_z.unit == 'km / (s Mpc)':
         print("Hubble constant seems to be in non-standard units...")
         set_trace()
-
+    print(f"Determined H(z) = {hubble_z.value:.2f} km/s/Mpc.")
     return hubble_z.value
 
  
